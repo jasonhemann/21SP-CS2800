27fee729b4c295942eb00c77bfed2572a0ad7491
Jason Hemann <jasonhemann@gmail.com>
2021-03-01 18:37:53 -0500

    pushing up the right version of 7b.lisp

--- _hw/hw7b.lisp
+++ _hw/hw7b.lisp
@@ -1,73 +1,265 @@
-#| 
+; ****************** BEGIN INITIALIZATION FOR ACL2s MODE ****************** ;
+; (Nothing to see here!  Your actual file is after this initialization code);
+(make-event
+ (er-progn
+  (set-deferred-ttag-notes t state)
+  (value '(value-triple :invisible))))
 
-HW 7a: Undecidability Results
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the CCG book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/ccg/ccg" :uncertified-okp nil :dir :system :ttags ((:ccg)) :load-compiled-file nil);v4.0 change
+
+;Common base theory for all modes.
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s base theory book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/base-theory" :dir :system :ttags :all)
+
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/custom" :dir :system :ttags :all)
+
+;; guard-checking-on is in *protected-system-state-globals* so any
+;; changes are reverted back to what they were if you try setting this
+;; with make-event. So, in order to avoid the use of progn! and trust
+;; tags (which would not have been a big deal) in custom.lisp, I
+;; decided to add this here.
+;; 
+;; How to check (f-get-global 'guard-checking-on state)
+;; (acl2::set-guard-checking :nowarn)
+(acl2::set-guard-checking :all)
+
+;Settings common to all ACL2s modes
+(acl2s-common-settings)
+;(acl2::xdoc acl2s::defunc) ;; 3 seconds is too much time to spare -- commenting out [2015-02-01 Sun]
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/acl2s-sigs" :dir :system :ttags :all)
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem setting up ACL2s mode.") (value :invisible))
+
+(acl2::xdoc acl2s::defunc) ; almost 3 seconds
+
+; Non-events:
+;(set-guard-checking :none)
+
+(set-inhibit-warnings! "Invariant-risk" "theory")
+
+(in-package "ACL2")
+(redef+)
+(defun print-ttag-note (val active-book-name include-bookp deferred-p state)
+  (declare (xargs :stobjs state)
+	   (ignore val active-book-name include-bookp deferred-p))
+  state)
+
+(defun print-deferred-ttag-notes-summary (state)
+  (declare (xargs :stobjs state))
+  state)
+
+(defun notify-on-defttag (val active-book-name include-bookp state)
+  (declare (xargs :stobjs state)
+	   (ignore val active-book-name include-bookp))
+  state)
+(redef-)
+
+(acl2::in-package "ACL2S")
+
+; ******************* END INITIALIZATION FOR ACL2s MODE ******************* ;
+;$ACL2s-SMode$;ACL2s
+
+#|
+
+HW 7b. More on Measures, Admission and Definition
 
 |#
 
-
-#| PART I: A READING ASSIGNMENT ABOUT BIG NUMBERS |# 				       
-
-;; This is a reading assignment. You will have to read, and be
-;; prepared to discuss, the following article:
-				       
-;; https://www.scottaaronson.com/writings/bignumbers.html
-				       
-;; You will (likely) have an in-class discussion based on this. So you
-;; should absolutely expect to have this understanding checked and
-;; verified.
-				       
-;; If they come up, you should also be prepared to answer some
-;; questions individually about this article. You do not need to write
-;; anything here, however.
-
-	
-#|  Part II: DIAGONALIZATION |# 
-
-;; 1. Using a diagonalization argument as described in section 5.4.2
-;; and used in 5.4.2 and 5.4.4, demonstrate the nondenumerability of
-;; the real numbers 0 <= r <= 1. Hint: you might find this especially
-;; elegant if you work in base 2. Like the examples from the book,
-;; this should look much closer to an "informal proof" from a math
-;; class than to an ACL2 or pen-and-paper proof.
-
 #| 
 
-Answer: 
+After a slight reprieve, I want to re-visit this measure function
+stuff, because we /will/ need it again.
+
+As a reminder, you are welcome to try these functions on ACL2s, but
+you cannot simply report what ACL2s reports to you. You have to
+provide your own measure arguments.
+
+
+1. There are many functions that are admissible, but ACL2s will not
+   admit them without help from the user. 
+
+2. It is possible that ACL2s has bugs! If so, it is possible that it
+   erroneously proves admissibility in some cases.
 
 |# 
 
-#| Part III: Undecidability of the Busy Beaver Problem. |# 
+;; You can ignore these; they adjust some ACL2s settings so that
+;; definec only uses the provide measure function to prove
+;; termination, and so that definec doesn't do as much magic as it
+;; would otherwise.
 
-;; You have now seen and understood proofs of the undecidability of
-;; the halting problem. You have encountered this at minimum in the
-;; course textbook, in the "Scooping ..." reading and as discussed in
-;; lecture.
-
-;; You have also now read and understood a great deal of the proof and
-;; history of the busy beaver problem. You should be able to argue its
-;; undecidability, via an informal proof. In fact, I don't think you
-;; would find that challenging at all.
-
-;; So, for a little added flavor, you will express a proof of the
-;; undecidabliity of the busy beaver problem in Seussian rhyme, a la
-;; "Scooping." Provide your answer below. You may use slant rhymes if
-;; you need, or regional accents if you must (e.g. "data"/"later").
-
-;; Please follow your Seussian proof with an explanation of it in
-;; plain English prose. Where not immediately, transparently obvious
-;; point us to what each piece is accomplishing. This should take you
-;; several stanzas---approximately the same length as "Scooping ...".
-
-;; Consider: if you do this well, you too could get your proof
-;; published!!
+(set-termination-method :measure)
+(set-well-founded-relation n<)
+(set-defunc-typed-undef nil)
+(set-defunc-generalize-contract-thm nil)
+(set-gag-mode nil)
 
 
 #| 
 
-Title: 
-
-Poem: 
-
-Commentary: 
+ Here is a function that you may find useful when constructing a
+ measure function for the magnitude function given below.
+ The ceil function is non-recursive, so you don't have to prove
+ its termination.
 
 |# 
+
+(definec ceil (x :non-neg-rational) :nat
+  (ceiling x 1))
+
+; Here are some tests showing that (how) ceil works.
+
+(check= (ceil 11) 11)
+(check= (ceil 10/3) 4)
+(check= (ceil 10/101) 1)
+
+;; 1. Come up with an acceptable measure function for each of the
+;; functions below. We once again use the (set-ignore-ok t) form.
+(set-ignore-ok t)
+
+
+(definec f1-measure
+
+  )
+
+(definec f2-measure
+
+  )
+
+(definec magnitude-measure
+
+  )
+
+;; B. Finish adding your measure to the function definition, to help
+;; ACL2 show termination. You can modify the function definition if
+;; you need, but make sure it's equivalent.
+
+(definec f1 (x :tl y :tl acc :tl) :tl
+  (declare (xargs :measure ))  
+  (cond
+    ((and (endp x) (endp y)) acc)
+    ((endp x) (f1 x (rest y) (cons (first y) acc))) ;; a
+    ((endp y) (f1 (rest x) y (cons (first x) acc))) ;; b
+    (t (f1 x nil (f1 nil y acc)))))                 ;; c 
+
+(definec f2 (x :tl y :tl acc :tl) :tl
+  (declare (xargs :measure ))
+   (if (endp x)
+       (if (endp y)
+	   acc
+	 (f2 x (rest y) (cons (first y) acc))) ;; a
+     (f2 (rest x) y (cons (first x) acc))))    ;; b
+
+
+;; Oh no! ACL2s is in trouble! It needs *YOUR* help!
+:program
+
+(definec magnitude (x :non-neg-rational) :integer
+  (declare (xargs :measure ))
+  (cond 
+    ((equal x 0) 0)
+    ((>= x 10) (+ 1 (magnitude (/ x 10)))) ;; a
+    ((< x 1) (* -1 (magnitude (/ 1 x))))   ;; b
+    (t 1)))
+
+#| 
+
+NB: You might need to understand how magnitude works. One way to do
+that is to "trace" it and try running it on examples.
+
+|# 
+(trace$ magnitude)
+
+(magnitude 20)
+(magnitude 200000)
+(magnitude 200000000000)
+(magnitude 1/20)
+(magnitude 1/200000)
+(magnitude 1/200000000000)
+
+(untrace$ magnitude)
+
+:logic
+
+;; C. State, as a quoted list, the contract theorem (condition 5).
+
+
+(defconst *f1-contract-theorem*
+
+  )
+
+(make-event `(thm ,*f1-contract-theorem*))
+
+
+(defconst *f2-contract-theorem*
+
+  )
+
+(make-event `(thm ,*f2-contract-theorem*))
+
+
+(defconst *magnitude-contract-theorem*
+
+  )
+
+(make-event `(thm ,*magnitude-contract-theorem*))
+
+;; D. Prove that the function is terminating using your measure
+;; function and equational reasoning for the recursive calls.
+
+
+Conjecture f1a-terminates:
+
+...
+
+QED
+
+
+Conjecture f1b-terminates:
+
+...
+
+QED
+
+
+Conjecture f1c-terminates:
+
+...
+
+QED
+
+
+
+Conjecture f2a-terminates:
+
+...
+
+QED
+
+
+
+Conjecture f2b-terminates:
+
+...
+
+QED
+
+
+Conjecture magnitudea-terminates:
+
+...
+
+QED
+
+
+Conjecture magnitudeb-terminates:
+
+...
+
+QED
+
